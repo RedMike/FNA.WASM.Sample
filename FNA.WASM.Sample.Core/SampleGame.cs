1877987bbdde2b1a1a2efa36a483cc73c474404a
Mike Roibu <contact@mikeroibu.com>
2024-03-02 21:10:24 +0000

    Finish setting up console sample

--- FNA.WASM.Sample.Core/SampleGame.cs
+++ FNA.WASM.Sample.Core/SampleGame.cs
@@ -1,14 +1,50 @@
-﻿namespace FNA.WASM.Sample.Core;
+﻿using Microsoft.Xna.Framework;
 
-public class SampleGame : IDisposable
+namespace FNA.WASM.Sample.Core;
+
+public class SampleGame : Game
 {
-    public void Dispose()
+    public SampleGame()
     {
-        // TODO release managed resources here
+        GraphicsDeviceManager gdm = new GraphicsDeviceManager(this);
+
+        // Typically you would load a config here...
+        gdm.PreferredBackBufferWidth = 1280;
+        gdm.PreferredBackBufferHeight = 720;
+        gdm.IsFullScreen = false;
+        gdm.SynchronizeWithVerticalRetrace = true;
+    }
+    
+    protected override void Initialize()
+    {
+        /* This is a nice place to start up the engine, after
+         * loading configuration stuff in the constructor
+         */
+        base.Initialize();
     }
 
-    public void Run()
+    protected override void LoadContent()
     {
-        throw new NotImplementedException();
+        // Load textures, sounds, and so on in here...
+        base.LoadContent();
+    }
+
+    protected override void UnloadContent()
+    {
+        // Clean up after yourself!
+        base.UnloadContent();
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        // Run game logic in here. Do NOT render anything here!
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        // Render stuff in here. Do NOT run game logic in here!
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+        base.Draw(gameTime);
     }
 }
