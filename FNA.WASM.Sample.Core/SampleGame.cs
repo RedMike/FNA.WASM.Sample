be66acde9b5d04b70e1ad62dbb377fc13e24e035
Mike Roibu <contact@mikeroibu.com>
2024-03-04 22:51:12 +0000

    Replace sample image with some Kenney assets and super basic sample logic

--- FNA.WASM.Sample.Core/SampleGame.cs
+++ FNA.WASM.Sample.Core/SampleGame.cs
@@ -11,7 +11,44 @@ public class SampleGame : Game
     private GamePadState _gamepadPrev = new GamePadState();
 
     private SpriteBatch _spriteBatch;
-    private Texture2D _texture;
+    private readonly Dictionary<string, Texture2D> _textures = new Dictionary<string, Texture2D>();
+
+    private class Entity
+    {
+        private const float LinearDrag = 0.01f;
+        private const float AngularDrag = 0.01f;
+        
+        public string Sprite { get; set; }
+        public Vector2 Position { get; set; }
+        public Vector2 DeltaPosition { get; set; }
+        public float Rotation { get; set; }
+        public float DeltaRotation { get; set; }
+        public float Scale { get; set; }
+
+        public Vector2? CachedAnchor { get; set; } = null;
+
+        public void Update(float deltaTime)
+        {
+            Position += DeltaPosition * deltaTime;
+            DeltaPosition *= 1.0f - (LinearDrag * deltaTime);
+
+            Rotation += DeltaRotation * deltaTime;
+            while (Rotation < 0.0f)
+            {
+                Rotation += 360.0f;
+            }
+            while (Rotation > 360.0f)
+            {
+                Rotation -= 360.0f;
+            }
+
+            DeltaRotation *= 1.0f - (AngularDrag * deltaTime);
+        }
+    }
+    private Entity? _ship;
+    private List<Entity> _meteors = new List<Entity>();
+
+    private Vector2 _viewportOffset = Vector2.Zero;
     
     public SampleGame()
     {
@@ -21,15 +58,45 @@ public class SampleGame : Game
         gdm.PreferredBackBufferHeight = 400;
         gdm.IsFullScreen = false;
         gdm.SynchronizeWithVerticalRetrace = true; //TODO: does this do anything on WebGL?
+        _viewportOffset = new Vector2(-gdm.PreferredBackBufferWidth / 2.0f, -gdm.PreferredBackBufferHeight / 2.0f);
 
         Content.RootDirectory = "/assets";
     }
     
     protected override void Initialize()
     {
-        /* This is a nice place to start up the engine, after
-         * loading configuration stuff in the constructor
-         */
+        var rnd = new Random();
+        _ship = new Entity()
+        {
+            Sprite = "ship1",
+            Position = Vector2.Zero,
+            DeltaPosition = Vector2.Zero,
+            Rotation = 0.0f,
+            DeltaRotation = 0.0f,
+            Scale = 0.5f
+        };
+        var wVar = 300;
+        var hVar = 200;
+        for (var i = 0; i < 10; i++)
+        {
+            var sprite = "meteor" + rnd.Next(1, 5);
+            var x = rnd.Next(-wVar, wVar);
+            var y = rnd.Next(-hVar, hVar);
+            var dx = rnd.Next(-1000, 1000) / 50.0f;
+            var dy = rnd.Next(-1000, 1000) / 50.0f;
+            var r = rnd.Next(0, 3600)/100.0f;
+            var dr = rnd.Next(-100, 100) / 10.0f;
+            _meteors.Add(new Entity()
+            {
+                Sprite = sprite,
+                Position = new Vector2(x, y),
+                DeltaPosition = new Vector2(dx, dy),
+                Rotation = r,
+                DeltaRotation = dr,
+                Scale = 0.2f
+            });
+        }
+        
         base.Initialize();
     }
 
@@ -37,13 +104,34 @@ public class SampleGame : Game
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-        _texture = Content.Load<Texture2D>("img/test1.png");
+        _textures["ship1"] = Content.Load<Texture2D>("img/ship1.png");
+        _textures["meteor1"] = Content.Load<Texture2D>("img/meteor1.png");
+        _textures["meteor2"] = Content.Load<Texture2D>("img/meteor2.png");
+        _textures["meteor3"] = Content.Load<Texture2D>("img/meteor3.png");
+        _textures["meteor4"] = Content.Load<Texture2D>("img/meteor4.png");
     }
 
     protected override void UnloadContent()
     {
-        _spriteBatch.Dispose();
-        _texture.Dispose();
+        try
+        {
+            _spriteBatch.Dispose();
+        }
+        catch (Exception)
+        {
+            //no issue
+        }
+        foreach (var pair in _textures)
+        {
+            try
+            {
+                pair.Value.Dispose();
+            }
+            catch (Exception)
+            {
+                //no issue
+            }
+        }
     }
 
     protected override void Update(GameTime gameTime)
@@ -70,6 +158,15 @@ public class SampleGame : Game
         _keyboardPrev = keyboard;
         _mousePrev = mouse;
         _gamepadPrev = gamepad;
+
+        if (_ship != null)
+        {
+            _ship.Update((float)gameTime.ElapsedGameTime.TotalSeconds);
+        }
+        foreach (var entity in _meteors)
+        {
+            entity.Update((float)gameTime.ElapsedGameTime.TotalSeconds);
+        }
         
         base.Update(gameTime);
     }
@@ -77,12 +174,36 @@ public class SampleGame : Game
     protected override void Draw(GameTime gameTime)
     {
         // Render stuff in here. Do NOT run game logic in here!
-        GraphicsDevice.Clear(Color.CornflowerBlue);
+        GraphicsDevice.Clear(Color.DarkBlue);
+
+        var cameraPos = _ship?.Position ?? Vector2.Zero;
+        cameraPos += _viewportOffset;
 
         _spriteBatch.Begin();
-        _spriteBatch.Draw(_texture, Vector2.Zero, Color.White);
+        if (_ship != null)
+        {
+            Draw(cameraPos, _ship);
+        }
+
+        foreach (var entity in _meteors)
+        {
+            Draw(cameraPos, entity);
+        }
         _spriteBatch.End();
         
         base.Draw(gameTime);
+    }
+
+    private void Draw(Vector2 cameraPos, Entity entity)
+    {
+        //TODO: camera rotation
+        if (entity.CachedAnchor == null)
+        {
+            var t = _textures[entity.Sprite];
+            entity.CachedAnchor = new Vector2(t.Width/2.0f, t.Height/2.0f);
+        }
+        var oPos = entity.Position - cameraPos;
+        _spriteBatch.Draw(_textures[entity.Sprite], oPos, null, Color.White, entity.Rotation, entity.CachedAnchor.Value,
+            Vector2.One * entity.Scale, SpriteEffects.None, 0.0f);
     }
 }
